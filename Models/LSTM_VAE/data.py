79a7ebb9e4983b54b21e3dffcf5c26575914630d
Paolo Magliano <paolo.magliano@outlook.it>
2024-07-20 19:31:02 +0200

    Avoid use meta data

--- Models/LSTM_VAE/data.py
+++ Models/LSTM_VAE/data.py
@@ -16,32 +16,8 @@ def point_adjust(window_label, k=0):
     else:
         return 0
 
-def get_df_action(filepaths_csv, filepaths_meta, action2int=None, delimiter=";"):
-    # Load dataframes
-    print("Loading data.")
-    # Make dataframes
-    # Some classes show the output boolean parameter as True rather than true. Fix here
-    dfs_meta = list()
-    for filepath in filepaths_meta:
-        df_m = pd.read_csv(filepath, sep=delimiter)
-        df_m.str_repr = df_m.str_repr.str.replace('True', 'true')
-        df_m['filepath'] = filepath
-        dfs_meta.append(df_m)
-
-
-    df_meta = pd.concat(dfs_meta)
-    df_meta.index = pd.to_datetime(df_meta.init_timestamp.astype('datetime64[ms]'), format="%Y-%m-%dT%H:%M:%S.%f")
-    df_meta['completed_timestamp'] = pd.to_datetime(df_meta.completed_timestamp.astype('datetime64[ms]'),
-                                                    format="%Y-%m-%dT%H:%M:%S.%f")
-    df_meta['init_timestamp'] = pd.to_datetime(df_meta.init_timestamp.astype('datetime64[ms]'),
-                                               format="%Y-%m-%dT%H:%M:%S.%f")
-
-    # Eventually reduce number of classes
-    # df_meta['str_repr'] = df_meta.str_repr.str.split('=', expand = True,n=1)[0]
-    # df_meta['str_repr'] = df_meta.str_repr.str.split('(', expand=True, n=1)[0]
-
-    actions = df_meta.str_repr.unique()
-    dfs = [pd.read_csv(filepath_csv, sep=";") for filepath_csv in filepaths_csv]
+def get_df(filepaths_csv, delimiter=";"):
+    dfs = [pd.read_csv(filepath_csv, sep=delimiter) for filepath_csv in filepaths_csv]
     df = pd.concat(dfs)
 
     # Sort columns by name !!!
@@ -54,56 +30,14 @@ def get_df_action(filepaths_csv, filepaths_meta, action2int=None, delimiter=";")
     df.drop(["machine_nameKuka Robot_export_active_energy",
              "machine_nameKuka Robot_import_reactive_energy"] + columns_to_drop, axis=1, inplace=True)
 
-    signals = df.columns
-
-    df_action = list()
-    for action in actions:
-        for index, row in df_meta[df_meta.str_repr == action].iterrows():
-            start = row['init_timestamp']
-            end = row['completed_timestamp']
-            df_tmp = df.loc[start: end].copy()
-            df_tmp['action'] = action
-            # Duration as string (so is not considered a feature)
-            df_tmp['duration'] = str((row['completed_timestamp'] - row['init_timestamp']).total_seconds())
-
-            df_action.append(df_tmp)
-    df_action = pd.concat(df_action, ignore_index=True)
-    df_action.index = pd.to_datetime(df_action.time.astype('datetime64[ms]'), format="%Y-%m-%dT%H:%M:%S.%f")
-    df_action = df_action[~df_action.index.duplicated(keep='first')]
-
-    # Drop NaN
-    df = df.dropna(axis=0)
-    df_action = df_action.dropna(axis=0)
-
-    if action2int is None:
-        action2int = dict()
-        j = 1
-        for label in df_action.action.unique():
-            action2int[label] = j
-            j += 1
-
-    df_merged = df.merge(df_action[['action']], left_index=True, right_index=True, how="left")
-    # print(f"df_merged len: {len(df_merged)}")
-    # Where df_merged in NaN Kuka is in idle state
-    df_idle = df_merged[df_merged['action'].isna()].copy()
-    df_idle['action'] = 'idle'
-    df_idle['duration'] = df_action.duration.values.astype(float).mean().astype(str)
-    df_action = pd.concat([df_action, df_idle])
-
-    # ile label must be 0 for debug mode
-    action2int['idle'] = 0
-    print(f"Found {len(set(df_action['action']))} different actions.")
-    print("Loading data done.\n")
-
-    return df_action, df, df_meta, action2int
+    return df
 
 def get_records(filepath, sampling, file_numbers):
     filepath_csv = [os.path.join(filepath, f"rec{r}_20220811_rbtc_{sampling}s.csv") for r in file_numbers]
-    filepath_meta = [os.path.join(filepath, f"rec{r}_20220811_rbtc_{sampling}s.metadata") for r in file_numbers]
-    df_action, _, _, _ = get_df_action(filepath_csv, filepath_meta)
-    df_action = df_action.drop(columns=['time', 'action', 'duration'])
+    df = get_df(filepath_csv)
+    df = df.drop(columns=['time', 'action', 'duration'])
 
-    return df_action
+    return df
 
 def get_collisions(filepath):
     collisions = pd.read_excel(os.path.join(filepath, "20220811_collisions_timestamp.xlsx"))
